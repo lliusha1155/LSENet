194ce6a637cb94c0bdfc1b277351e84c092f226b
HaoGuo <55483751+lliusha1155@users.noreply.github.com>
2021-06-13 18:15:03 +0800

    Add files via upload

---
+++ attention_map_visualization.py
@@ -0,0 +1,184 @@
+from keras.models import *
+from keras.layers import *
+from keras.activations import *
+import keras.backend as K
+import keras
+from keras.optimizers import Adam
+from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, LambdaCallback
+from PIL import Image
+import tensorflow as tf
+import cv2
+import numpy as np
+import os
+import copy
+import random
+import matplotlib.pyplot as plt
+from Model.LSENet import backbone
+from Model.LSENet import head
+
+log_dir = 'logs/'
+class_colors = [[0,0,0],[255,0,0],[255,255,255],[255,102,0],[0,255,255],[255,255,0],[153,255,0] # 分别为 黑色， 红色，白红，橙色，
+                ,[255,153,0],[0,0,255],[102,0,255],[255,0,255],[139,35,35]]     # 蓝绿，黄色，黄绿，橙黄，蓝色，蓝紫，紫色，棕色
+
+NCLASSES = 12
+HEIGHT = 352
+WIDTH = 352
+down_sample_size = 11
+batch_size = 1
+
+# get model
+img_input = Input(shape=(HEIGHT,WIDTH , 3 ))
+season_input = Input(shape=(12,))
+feats = backbone(img_input,season_input,HEIGHT,WIDTH)
+o = head(feats,NCLASSES, HEIGHT, WIDTH, down_sample_size,batch_size)
+model = Model(inputs=[img_input,season_input],outputs= o)
+
+#********************************************************************#
+model.load_weights(log_dir + '71_25.h5')
+#********************************************************************#
+
+# source image
+# jpg = '20180623.png'
+jpg = '20180114.png'
+
+# attention_map_class
+class_pick = 0
+
+img = Image.open("dataset/trainX/"+jpg[:12])
+old_img = copy.deepcopy(img)
+old_img = np.array(old_img)[:,:,0:3]
+old_img = Image.fromarray(np.uint8(old_img))
+
+img = np.array(img)[:,:,0:3]
+
+img = img/255
+img = np.pad(img, ((0, 12), (0, 92), (0, 0)), 'constant', constant_values=0)
+    
+img = img.reshape(-1,HEIGHT,WIDTH,3)
+    
+#seasonal feature input,one-hot
+season_name = int(jpg[4:6])
+season_label = np.zeros((1,12,))
+season_label[0,int(season_name)-1] = 1
+    
+pr = model.predict([img,season_label])[0]
+
+#-----------------------get output of attention layer-----------------------
+
+layer_name = 'pos_attention_down'
+intermediate_layer_model = Model(inputs=model.input,
+                                 outputs=model.get_layer(layer_name).output)
+pr = intermediate_layer_model.predict([img,season_label])[0]
+attention_down_ori = pr
+attention_down = pr[:31,:24]
+
+layer_name = 'pos_attention'
+intermediate_layer_model = Model(inputs=model.input,
+                                 outputs=model.get_layer(layer_name).output)
+pr = intermediate_layer_model.predict([img,season_label])[0]
+attention = pr[:340,:260]
+
+#-----------------------find edge of the ocean front------------------------------
+img = Image.open(r"dataset/trainY" + '/' + jpg)
+img = np.array(img)
+edge_img = np.zeros((340,260))
+edge_labels = np.zeros((340,260,NCLASSES))
+
+# latitude scan
+for i in range(340):
+    for j in range(1,12):
+        index = np.argwhere(img[i] == j) # Get all of the indexes in target class
+        if index.shape != (0,1):         # If there is a index
+            edge_img_row = np.zeros((20,260)) # default maximum 20 consecutive arrays, each array up to 260 pixels
+            seq_num = 0 
+            if index.shape[0]==1:
+                edge_img[i,int(index[0,0])] = j
+            else:
+                index_temp = np.squeeze(index)
+                for k in range(index_temp.shape[0]-1):
+                    diff = index_temp[k+1] - index_temp[k]
+                    if diff == 1:
+                            edge_img_row[seq_num, k] = index_temp[k]      
+                            edge_img_row[seq_num, k+1] = index_temp[k+1]  
+                    else:
+                            edge_img_row[seq_num, k] = index_temp[k]
+                            seq_num = seq_num + 1
+                            edge_img_row[seq_num, k+1] = index_temp[k+1]
+            
+                for l in range(20):
+                    a = edge_img_row[l]
+                    if np.any(a!=0): 
+                        minval = np.min(a[np.nonzero(a)])
+                        maxval = np.max(a[np.nonzero(a)])
+                    
+                        #填数
+                        edge_img[i,int(minval)] = j
+                        edge_img[i,int(maxval)] = j
+                        
+#longitudinal scanning
+for i in range(260):
+    for j in range(1,12):
+        index = np.argwhere(img[:,i] == j) #Get all of the indexes in target class
+        if index.shape != (0,1):         # If there is a index
+            edge_img_row = np.zeros((20,340)) # default maximum 20 consecutive arrays, each array up to 260 pixels
+            seq_num = 0 
+            if index.shape[0]==1:
+                edge_img[int(index[0,0]),i] = j
+            else:
+                index_temp = np.squeeze(index)
+                for k in range(index_temp.shape[0]-1):
+                    diff = index_temp[k+1] - index_temp[k]
+                    if diff == 1:
+                            edge_img_row[seq_num, k] = index_temp[k]      
+                            edge_img_row[seq_num, k+1] = index_temp[k+1]  
+                    else:
+                            edge_img_row[seq_num, k] = index_temp[k]
+                            seq_num = seq_num + 1
+                            edge_img_row[seq_num, k+1] = index_temp[k+1]
+            
+                for l in range(20):
+                    a = edge_img_row[l]
+                    if np.any(a!=0): 
+                        minval = np.min(a[np.nonzero(a)])
+                        maxval = np.max(a[np.nonzero(a)])
+                    
+                        edge_img[int(minval),i] = j
+                        edge_img[int(maxval),i] = j
+                        
+for c in range(NCLASSES):
+    edge_labels[: , : , c ] = (edge_img[:,:] == c ).astype(int)
+
+edge = edge_labels[:,:,class_pick]
+
+save_path = 'attention_map'
+
+
+if class_pick!= 0:
+    a = np.argwhere(edge==1)
+    x = a[:,0]
+    y = a[:,1]
+    #plot
+    fig = plt.figure(figsize=(5.0,6.85))      #size
+    plt.xticks([])  # Remove the x-axis values
+    plt.yticks([])  # Remove the y-axis values
+    #attention
+    con = plt.pcolormesh(attention[:,:,class_pick], cmap = 'coolwarm')
+    ax = plt.gca()
+    plt.scatter(y, x, s=2, c='g')
+    ax.invert_yaxis()
+    cbar = plt.colorbar(con,orientation='horizontal', fraction=0.05, pad=0.03)
+    plt.savefig(save_path +'/'+ jpg[:8] + '_class' +str(class_pick) + '.png', dpi = 300, bbox_inches = 'tight',pad_inches = 0)
+else:
+    edge = edge_img
+    a = np.argwhere(edge!=0)
+    x = a[:,0]
+    y = a[:,1]
+    fig = plt.figure(figsize=(5.0,6.85))      
+    plt.xticks([])  
+    plt.yticks([])  
+    con = plt.pcolormesh(attention[:,:,class_pick], cmap = 'coolwarm')
+    ax = plt.gca()
+    plt.scatter(y, x, s=2, c='g')
+    ax.invert_yaxis()
+    cbar = plt.colorbar(con,orientation='horizontal', fraction=0.05, pad=0.03)
+    plt.savefig(save_path +'/'+ jpg[:8] + '_class' +str(class_pick) + '.png', dpi = 300, bbox_inches = 'tight',pad_inches = 0)
---
+++ test.py
@@ -0,0 +1,160 @@
+from keras.models import *
+from keras.layers import *
+from keras.activations import *
+import keras.backend as K
+import keras
+from keras.optimizers import Adam
+from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, LambdaCallback
+from PIL import Image
+import tensorflow as tf
+import cv2
+import numpy as np
+import os
+import copy
+import random
+import matplotlib.pyplot as plt
+import pandas as pd
+import json
+from tools.image_augment import PhotometricDistort,RandomCrop,RandomFlipImage
+from Model.LSENet import backbone
+from Model.LSENet import head
+
+#-----------------------visulization detection results-----------------------
+log_dir = 'logs/'
+class_colors = [[0,0,0],[255,0,0],[255,255,255],[255,102,0],[0,255,255],[255,255,0],[153,255,0] 
+                ,[255,153,0],[0,0,255],[102,0,255],[255,0,255],[139,35,35]]     
+
+def month2season(num):
+    season_label = np.zeros((1,4))
+    
+    if num>= 3 and num <= 5:
+        season_label[0,0] = 1     #Spring
+    elif num>=6 and num<=8:
+        season_label[0,1] = 1     #Summer
+    elif num>=9 and num<=11:
+        season_label[0,2] = 1     #Fall
+    else:
+        season_label[0,3] = 1     #Winter
+    return season_label
+
+NCLASSES = 12
+HEIGHT = 352
+WIDTH = 352
+down_sample_size = 11
+batch_size = 1
+
+# get the model
+img_input = Input(shape=(HEIGHT,WIDTH , 3 ))
+season_input = Input(shape=(12,))
+feats = backbone(img_input,season_input,HEIGHT,WIDTH)
+o = head(feats,NCLASSES, HEIGHT, WIDTH, down_sample_size,batch_size)
+model = Model(inputs=[img_input,season_input],outputs= o)
+
+#********************************************************************#
+model.load_weights(log_dir + '71_25.h5')
+#********************************************************************#
+
+#get the test set
+with open(r"dataset/test_list.txt","r") as f:
+    lines = f.readlines()
+
+save_path = 'img_out/'
+
+for jpg in lines:
+    img = Image.open("dataset/trainX/"+jpg[:12])
+    old_img = copy.deepcopy(img)
+    old_img = np.array(old_img)[:,:,0:3]
+    old_img = Image.fromarray(np.uint8(old_img))
+                                                        
+    img = np.array(img)[:,:,0:3]
+
+    img = img/255
+    img = np.pad(img, ((0, 12), (0, 92), (0, 0)), 'constant', constant_values=0)
+    
+    img = img.reshape(-1,HEIGHT,WIDTH,3)
+    
+    #seasonal feature input ,one-hot
+    season_name = int(jpg[4:6])
+    season_label = np.zeros((1,12,))
+    season_label[0,int(season_name)-1] = 1
+    #season_label = month2season(season_name)
+
+    pr = model.predict([img,season_label])[0]
+
+    pr = pr.reshape((HEIGHT,WIDTH,NCLASSES)).argmax(axis=-1)
+
+    seg_img = np.zeros((HEIGHT, WIDTH,3))
+    colors = class_colors
+
+    for c in range(NCLASSES):
+        seg_img[:,:,0] += ( (pr[:,: ] == c )*( colors[c][0] )).astype('uint8')
+        seg_img[:,:,1] += ((pr[:,: ] == c )*( colors[c][1] )).astype('uint8')
+        seg_img[:,:,2] += ((pr[:,: ] == c )*( colors[c][2] )).astype('uint8')
+    
+    seg_img = seg_img[:340,:260,:]
+    seg_img = Image.fromarray(np.uint8(seg_img))
+
+    image = Image.blend(old_img,seg_img,0.7)
+    image.save(save_path + jpg[:12])
+    
+#-----------------------compute mIoU-----------------------
+
+#confusion matrix
+def fast_hist(a, b, n):#a is ground truth，shape(H×W,)；b is the predict label，shape(H×W,)；n is number of classes
+    k = (a >= 0) & (a < n) 
+    return np.bincount(n * a[k].astype(int) + b[k], minlength=n ** 2).reshape(n, n)
+
+def per_class_iu(hist):#caculate mIoU
+    return np.diag(hist) / (hist.sum(1) + hist.sum(0) - np.diag(hist))
+def compute_mIoU( lines, num_classes):
+    """
+    Compute IoU given the predicted colorized images and 
+    """
+    print('Num classes', num_classes)
+    name_classes = ["background","front1","front2","front3","front4","front5","front6","front7","front8","front9","front10","front11"]
+    hist = np.zeros((num_classes, num_classes))
+    pix_acc_total = 0
+    pix_acc_num = 0
+
+    for jpg in lines:
+        truth = np.array(Image.open('dataset/trainY/' + jpg[:12]))
+        
+        img = Image.open('dataset/trainX/' + jpg[:12]) 
+        img = np.array(img)[:,:,:3]
+
+        img = img/255
+        img = np.pad(img, ((0, 12), (0, 92), (0, 0)), 'constant', constant_values=0)
+    
+        img = img.reshape(-1,HEIGHT,WIDTH,3)
+        
+        #seasonal featur input,one-hot
+        season_name = int(jpg[4:6])
+        season_label = np.zeros((1,12,))
+        season_label[0,int(season_name)-1] = 1
+        #season_label = month2season(season_name)
+
+        pr = model.predict([img,season_label])[0]
+        
+        pr = pr.reshape((HEIGHT,WIDTH,NCLASSES)).argmax(axis=-1)
+        pr_img = pr[:340,:260]
+        
+        pix_acc = np.sum((truth== pr_img).astype('int'))/(truth.shape[0]*truth.shape[1])
+        pix_acc_total = pix_acc_total + pix_acc
+        pix_acc_num = pix_acc_num + 1
+        hist += fast_hist(truth.flatten(), pr_img.flatten(), num_classes)
+    
+    mIoUs = per_class_iu(hist)
+    
+    for ind_class in range(num_classes):#output mIoU
+        print('===>' + name_classes[ind_class] + ':\t' + str(round(mIoUs[ind_class] * 100, 2)))
+    print('===> mIoU: ' + str(round(np.nanmean(mIoUs) * 100, 2)))
+    
+    print('Pixel Acc = ' + str(np.around(100 * pix_acc_total/pix_acc_num, 2)) + '%')
+    return mIoUs    
+
+if __name__ == "__main__":
+    log_dir = 'logs/'
+    HEIGHT=352
+    WIDTH= 352
+    NCLASSES = 12
+    compute_mIoU(lines, NCLASSES)
